a445f59d5148111a1804e6603cfed385ce5b9fcf
Alan Pitcher <alanbenpitcher@gmail.com>
2026-04-29 19:44:04 -0400

    added weapon struct

---
+++ src/game/weapon.c
@@ -0,0 +1,6 @@
+//
+// Created by Alan Pitcher on 4/29/2026.
+//
+
+#include <stdio.h>
+#include "game/weapon.h"
---
+++ src/game/weapon.h
@@ -0,0 +1,8 @@
+//
+// Created by Alan Pitcher on 4/29/2026.
+//
+
+#ifndef WEAPON_H
+#define WEAPON_H
+
+#endif //WEAPON_H
